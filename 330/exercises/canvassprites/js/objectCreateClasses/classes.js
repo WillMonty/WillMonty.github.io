6d4a9c4e224353180891e019d58498b615933d07
William Montgomery <willgmontgomery@gmail.com>
2018-02-22 14:12:40 -0500

    Finished ES6 Classes Exercise

---
+++ 330/exercises/canvassprites/js/objectCreateClasses/classes.js
@@ -0,0 +1,159 @@
+"use strict";
+class Sprite{
+
+	constructor(){
+		this.x = 0;
+		this.y = 0;
+		this.fwd = {};
+		this.speed = 0;
+	}
+
+	move()
+	{
+		this.x += this.fwd.x * this.speed;
+		this.y += this.fwd.y * this.speed;
+	}
+
+	reflectX()
+	{
+		this.fwd.x *= -1;
+	}
+
+	reflectY()
+	{
+		this.fwd.y *= -1;
+	}
+}
+
+class CircleSprite extends Sprite{
+	constructor(rect={left,top,width,height}, radius = 20, color = 'red'){
+		super();
+		this.radius = radius;
+		this.color = color;
+		this.x = Math.random() * rect.width + rect.left;
+		this.y = Math.random() * rect.height + rect.top;
+		this.fwd = getRandomUnitVector();
+		this.speed = 2;
+	}
+
+	draw(ctx){
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+		ctx.closePath();
+		ctx.fillStyle = this.color;
+		ctx.fill();
+		ctx.restore();
+	}
+}
+
+class SquareSprite extends CircleSprite{
+	constructor(rect={left,top,width,height}, width = 25, height = 25, color = 'red'){
+		super(rect, width, color);
+		this.width = width;
+		this.height = height;
+	}
+
+	draw(ctx){
+		ctx.save();
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.restore();
+	}
+}
+
+class ImageSprite extends SquareSprite{
+	constructor(rect={left,top,width,height}, width = 50, height = 50, url='images/Sean.png'){
+
+		super(rect, width, height, 'red');
+		this.image = new Image(width, height);
+		this.image.src = url;
+	}
+
+	draw(ctx){
+		ctx.save();
+		ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+		ctx.restore();
+	}
+}
+
+class ImageShrinkSprite extends ImageSprite{
+	constructor(rect={left,top,width,height}, width = 50, height = 50, shrinkRate = 10, url='images/Sean.png'){
+		super(rect, width, height, url);
+		this.image = new Image(width, height);
+		this.image.src = url;
+		this.shrinkRate = shrinkRate;
+	}
+	
+	reflectX(){
+		this.fwd.x *= -1;
+		this.width = this.width - this.shrinkRate;
+		this.height = this.height - this.shrinkRate;
+	}
+
+	reflectY(){
+		this.fwd.y *= -1;
+		this.width = this.width - this.shrinkRate;
+		this.height = this.height - this.shrinkRate;
+	}
+
+	draw(ctx){
+
+		if(this.width <= 0 || this.height <= 0)
+		{
+			this.speed = 0;
+			this.fwd.x = 0;
+			this.fwd.y = 0;
+			return;            
+		}
+
+		ctx.save();
+		ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+		ctx.restore();
+
+	}
+}
+
+function createCircleSprites(num=20, rect={left:0,top:0,width:300,height:300}, radius=20, color='red'){
+	let sprites = [];
+	for(let i=0;i<num;i++){
+
+		let s = new CircleSprite(rect, radius, color);
+		sprites.push(s);
+	}
+
+	return sprites; 
+}
+
+function createSquareSprites(num=20,rect={left:0,top:0,width:300,height:300}, width=25, height=25, color='red'){
+	let sprites = [];
+	for(let i=0;i<num;i++){
+
+		let s = new SquareSprite(rect, width, height, color);
+		sprites.push(s);
+	}
+
+	return sprites; 
+}
+
+function createImageSprites(num=20,rect={left:0,top:0,width:300,height:300}, width=50, height=50, url='images/Sean.png'){
+	let sprites = [];
+	for(let i=0;i<num;i++){
+
+		let s = new ImageSprite(rect, width, height, url);
+		sprites.push(s);
+	}
+
+	return sprites; 
+}
+
+//Makes an image that shrinks itself every bounce. Eventually stops drawing when small enough
+function createImageShrinkSprites(num=20,rect={left:0,top:0,width:300,height:300}, width=50, height=50, shrinkRate=10, url='images/Sean.png'){
+	let sprites = [];
+	for(let i=0;i<num;i++){
+
+		let s = new ImageShrinkSprite(rect, width, height, shrinkRate, url);
+		sprites.push(s);
+	}
+
+	return sprites; 
+}
---
+++ 330/exercises/canvassprites/js/objectCreateClasses/main.js
@@ -0,0 +1,75 @@
+"use strict";
+// these variables are in "Script scope" and will be available in this and other .js files
+const ctx = document.querySelector("canvas").getContext("2d");
+const screenWidth = 600;
+const screenHeight = 400;
+let sprites = [];
+
+
+init();
+
+function init(){
+    let margin = 50;
+    let rect = {left: margin, top: margin, width: screenWidth - margin*2, height: screenHeight-100*2}
+    sprites = sprites.concat(createCircleSprites(5, rect, 10, 'orange'),
+                             createCircleSprites(5, rect, 15, 'green'),
+                             createSquareSprites(3,rect, 20, 10, 'yellow'),
+                             createSquareSprites(1,rect, 30, 30, 'white'),
+                             createImageSprites(1, rect, 70, 70, 'images/reddit.png'),
+                             createImageSprites(4, rect, 30, 30, 'images/downvote.png'),
+                             createImageSprites(7, rect, 60, 60,
+                             'images/ealogo.png'),
+                             createImageShrinkSprites(10, rect, 70, 70, 10, 'images/money.png'),
+                             createImageShrinkSprites(3, rect, 150, 50, 10, 'images/bf2logo.png')
+                            );
+    loop();
+}
+
+function loop(){
+    // schedule a call to loop() in 1/60th of a second
+    requestAnimationFrame(loop);
+
+    // draw background
+	ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,screenWidth,screenHeight)
+
+    // loop through sprites
+    for (let s of sprites){
+        // move sprites
+        s.move();
+
+        // check sides and bounce
+        if(s.radius)
+        {
+            // a circle
+            if (s.x <= s.radius || s.x >= screenWidth-s.radius){
+                s.reflectX();
+                s.move();
+            }
+            if (s.y <= s.radius || s.y >= screenHeight-s.radius){
+                s.reflectY();
+                s.move();
+            }
+        }
+        else
+        { // `s` is NOT a circle
+            // left and right
+            if (s.x <= 0 || s.x >= screenWidth-s.width){
+                s.reflectX();
+                s.move();
+            }
+
+            // top and bottom
+            if(s.y <=0 || s.y > screenHeight - s.height)
+            {
+                s.reflectY();
+                s.move();
+            }
+
+        } // end if s.radius
+
+        // draw sprites
+        s.draw(ctx);
+
+    } // end for
+} // end loop()

58213b211c554de2750004673675511f88ac7fc3
William Montgomery <willgmontgomery@gmail.com>
2018-03-21 17:44:39 -0400

    2Wild2West Prototype 2.

--- 330/projects/game/js/cowboy.js
+++ 330/projects/game/js/cowboy.js
@@ -1,103 +1,230 @@
 import { gamepads as gp, applyDeadzone } from './controllers.js';
 import { bullet } from './bullet.js';
-import { b, app, AddBullet } from './loader.js';
+import { app } from './loader.js';
+import { b, AddBullet } from './manager.js';
 export { Cowboy };
 
 //Variables to modify both players' behavior
 let maxSpeed = 8;
 let deadzone = 0.15;
 let crossRange = 125;
-let lastState = 0;
+let animationFPS = 15;
 
 class Cowboy {
-    constructor(pNum) {
-        this.sprite = new PIXI.Sprite(PIXI.loader.resources["hat"].texture);
-        this.sprite.anchor.set(0.5, 0.5);
-        this.sprite.position.x = 50;
-        this.sprite.position.y = 30;
-        b.addCollisionProperties(this.sprite);
+	constructor(pNum) {
+		//Sprite settings
+		this.sprite = new PIXI.Sprite();
+		this.HP = 1;
+		this.sprite.anchor.set(0.5, 0.5);
+		b.addCollisionProperties(this.sprite);
 
-        this.crosshair = new PIXI.Sprite(PIXI.loader.resources['crosshair'].texture);
-        this.crosshair.anchor.set(0.5, 0.5);
+		//Set up crosshair sprite
+		this.crosshair = new PIXI.Sprite();
+		this.crosshair.anchor.set(0.5, 0.5);
+		this.sprite.addChild(this.crosshair);
 
-        this.sprite.addChild(this.crosshair);
+		//Player number and color
+		this.playerNum = pNum;
+		this.gamepad = gp[pNum - 1];
 
-        this.playerNum = pNum;
-        this.gamepad = gp[pNum - 1];
+		//Set player number dependent variables
+		if (this.playerNum == 1) {
+			this.color = 'white';
 
-        this.velocity = {};
-        this.velocity.x = 0;
-        this.velocity.y = 0;
-        this.fwd = {};
-        this.fwd.x = 0;
-        this.fwd.y = 0;
-    }
+			//Set spawn position and direction
+			this.sprite.position.x = 100;
+			this.sprite.position.y = app.renderer.height/2;
+			this.lastDirection = 'E';
+			
+			this.crosshair.texture = PIXI.loader.resources['white crosshair'].texture;
 
-    //Update the cowboy object
-    update(delta) {
-        if (delta === undefined) {
-            return;
-        }
+			this.shootSound = new Howl({
+				src: ['./audio/shoot1.mp3']
+			});
+		} else {
+			this.color = 'black';
 
-        //Movement
-        this.fwd.x = applyDeadzone(this.gamepad.axes[0], deadzone);
-        this.fwd.y = applyDeadzone(this.gamepad.axes[1], deadzone);
+			this.sprite.position.x = app.renderer.width - 100;
+			this.sprite.position.y = app.renderer.height/2;
+			this.lastDirection = 'W';
 
-        this.velocity.x = this.fwd.x * maxSpeed;
-        this.velocity.y = this.fwd.y * maxSpeed;
+			this.crosshair.texture = PIXI.loader.resources['black crosshair'].texture;
 
-        this.sprite.position.x += this.velocity.x * delta;
-        this.sprite.position.y += this.velocity.y * delta;
+			this.shootSound = new Howl({
+				src: ['./audio/shoot2.mp3']
+			});
+		}
 
-        //Crosshair position
-        this.crosshair.position.x = applyDeadzone(this.gamepad.axes[2], deadzone) * crossRange;
+		//Set initial texture
+		this.sprite.texture = PIXI.loader.resources[
+			this.color + '_' + this.lastDirection + '_' + 'idle0'
+		].texture;
 
-        this.crosshair.position.y = applyDeadzone(this.gamepad.axes[3], deadzone) * crossRange;
+		//Movement vars
+		this.velocity = {};
+		this.velocity.x = 0;
+		this.velocity.y = 0;
 
-        if (this.crosshair.position.x == 0 && this.crosshair.position.y == 0) {
-            this.crosshair.visible = false;
-        } else {
-            this.crosshair.visible = true;
-        }
+		this.fwd = {}; //The direction the player is currently pointed towards
+		this.fwd.x = 0;
+		this.fwd.y = 0;
 
-        //Shooting
-        if (this.gamepad.buttons[7].value > 0.3 && lastState < 0.3) {
-            this.shoot();
-        }
-        lastState = this.gamepad.buttons[7].value;
-        this.keepInBounds();
-    }
+		//Shooting vars
+		this.lastTriggerState = 0; //Last state of the trigger used to shoot
+		this.isAiming = false; //Determines if the player is actively aiming the crosshair
 
-    //Keeps the cowboy in bounds
-    keepInBounds() {
-        let x = this.sprite.position.x;
-        let y = this.sprite.position.y;
-        let spriteHalfWidth = this.sprite.width / 2;
-        let spriteHalfHeight = this.sprite.height / 2;
-        let stageWidth = app.renderer.width;
-        let stageHeight = app.renderer.height;
+		//Animation vars
+		this.frameNum = 9; //Frame the animation is on. 9 is the start, 0 is the end.
+		this.currFrameTime = 0; //Time this frame has been displaying
+		this.isShooting = false; //Is the shoot animation happening?
 
-        if (x - spriteHalfWidth <= 0)
-            this.sprite.position.x = spriteHalfWidth;
+		//Audio vars
+		this.reloadSound = new Howl({
+			src: ['./audio/reload.mp3'],
+			volume: 0.5,
+			loop: true
+		});
+	}
 
-        if (x + spriteHalfWidth >= stageWidth)
-            this.sprite.position.x = stageWidth - spriteHalfWidth;
+	//Update the cowboy object
+	update(delta) {
+		if (delta === undefined)
+			return;
 
-        //Add the same padding that the other bounds have
-        if (y + spriteHalfHeight >= stageHeight - 10)
-            this.sprite.position.y = stageHeight - spriteHalfHeight - 10;
+		if (this.gamepad === undefined)
+			return;
 
-        if (y - spriteHalfHeight <= 0)
-            this.sprite.position.y = spriteHalfHeight;
-    }
+		//Movement
+		if (!this.isShooting) {
+			this.fwd.x = applyDeadzone(this.gamepad.axes[0], deadzone);
+			this.fwd.y = applyDeadzone(this.gamepad.axes[1], deadzone);
 
-    shoot() {
+			this.velocity.x = this.fwd.x * maxSpeed;
+			this.velocity.y = this.fwd.y * maxSpeed;
 
-        let bulletFwd = {};
-        bulletFwd.x = applyDeadzone(this.gamepad.axes[2], deadzone);
-        bulletFwd.y = applyDeadzone(this.gamepad.axes[3], deadzone);
-        let bulletClone = new bullet(this, bulletFwd);
+			this.sprite.position.x += this.velocity.x * delta;
+			this.sprite.position.y += this.velocity.y * delta;
+		}
 
-        AddBullet(bulletClone);
-    }
+		//Crosshair position
+		this.crosshair.position.x = applyDeadzone(this.gamepad.axes[2], deadzone) * crossRange;
+
+		this.crosshair.position.y = applyDeadzone(this.gamepad.axes[3], deadzone) * crossRange;
+
+		if (this.crosshair.position.x == 0 && this.crosshair.position.y == 0) {
+			this.crosshair.visible = false;
+			this.isAiming = false;
+		} else {
+			this.crosshair.visible = true;
+			this.isAiming = true;
+		}
+
+		//Shooting (uses Right Trigger)
+		if (this.gamepad.buttons[7].value > 0.5 && this.lastTriggerState < .3 && this.isAiming) {
+			this.shoot();
+			this.frameNum = 2; //First frame number of the shoot animation
+		}
+		this.lastTriggerState = this.gamepad.buttons[7].value;
+
+		this.animate(delta); //Update sprite texture
+
+		this.keepInBounds();
+	}
+
+	animate(delta) {
+		//Find what direction the desired stick is in
+		let currDirection = '';
+		let action; //The action the cowboy is doing
+
+		if (this.isShooting) {
+			action = 'shoot';
+			currDirection = this.getCardinalStickDirection(3, 2);
+			if (currDirection == '')
+				currDirection = this.lastDirection;
+		} else {
+			currDirection = this.getCardinalStickDirection(1, 0);
+
+			if (currDirection == '') {
+				action = 'idle';
+				this.frameNum = 0;
+				currDirection = this.lastDirection;
+			} else {
+				this.lastDirection = currDirection;
+				action = 'walk';
+			}
+		}
+
+		//Set the texture based on the color, direction, action, and frame number
+		this.sprite.texture = PIXI.loader.resources[
+			this.color + '_' + currDirection + '_' + action + this.frameNum
+		].texture;
+
+		this.currFrameTime += delta;
+
+		//Should the next frame cycle in?
+		if (this.currFrameTime >= 60 / animationFPS) {
+			if (this.frameNum <= 0) {
+				this.frameNum = 9; //Reset cycle back to top
+				this.isShooting = false; //Reset shooting
+			} else {
+				this.frameNum--;
+			}
+
+			this.currFrameTime = 0; //Reset time for next frame
+		}
+	}
+
+	shoot() {
+		//this.shootSound.stop();
+		this.isShooting = true;
+		
+		let bulletFwd = {};
+		bulletFwd.x = applyDeadzone(this.gamepad.axes[2], deadzone);
+		bulletFwd.y = applyDeadzone(this.gamepad.axes[3], deadzone);
+		let bulletClone = new bullet(this, bulletFwd);
+
+		this.shootSound.play();
+		AddBullet(bulletClone);
+	}
+
+	//Keeps the cowboy in the game's bounds
+	keepInBounds() {
+		let x = this.sprite.position.x;
+		let y = this.sprite.position.y;
+		let spriteHalfWidth = this.sprite.width / 2;
+		let spriteHalfHeight = this.sprite.height / 2;
+		let stageWidth = app.renderer.width;
+		let stageHeight = app.renderer.height;
+
+		if (x - spriteHalfWidth <= 0)
+			this.sprite.position.x = spriteHalfWidth;
+
+		if (x + spriteHalfWidth >= stageWidth)
+			this.sprite.position.x = stageWidth - spriteHalfWidth;
+
+		//Add the same padding that the other bounds have
+		if (y + spriteHalfHeight >= stageHeight - 10)
+			this.sprite.position.y = stageHeight - spriteHalfHeight - 10;
+
+		if (y - spriteHalfHeight <= 0)
+			this.sprite.position.y = spriteHalfHeight;
+	}
+
+	//Gets cardinal direction of analog stick's vertical and horizontal axes
+	getCardinalStickDirection(vertical, horizontal) {
+		let horizontalDir = '';
+		let verticalDir = '';
+
+		if (applyDeadzone(this.gamepad.axes[vertical], deadzone) > 0)
+			verticalDir = 'S';
+		if (applyDeadzone(this.gamepad.axes[vertical], deadzone) < 0)
+			verticalDir = 'N';
+		if (applyDeadzone(this.gamepad.axes[horizontal], deadzone) > 0)
+			horizontalDir = 'E';
+		if (applyDeadzone(this.gamepad.axes[horizontal], deadzone) < 0)
+			horizontalDir = 'W';
+
+		return verticalDir + horizontalDir;
+	}
+
 }
--- 330/projects/game/js/loader.js
+++ 330/projects/game/js/loader.js
@@ -1,16 +1,19 @@
-import { Bump } from './bump.js';
-import { pollGamepads, setGamepadConnectionEvents, numPads } from './controllers.js';
-import { Cowboy } from './cowboy.js';
-export { b, app, AddBullet };
+import { startManager, gameLoop } from './manager.js';
+export { app };
 
-let players = []; //Holds the player objects
+WebFont.load({
+	custom: {
+		families: ['Edmunds'],
+		urls: ['./font/fonts.css']
+	}
+});
 
 let app = new PIXI.Application({
-    width: 256,
-    height: 256,
-    antialias: true,
-    transparent: false,
-    resolution: 1
+	width: 256,
+	height: 256,
+	antialias: true,
+	transparent: false,
+	resolution: 1
 });
 
 app.renderer.view.style.position = "absolute";
@@ -18,151 +21,60 @@ app.renderer.view.style.display = "block";
 app.renderer.autoResize = true;
 app.renderer.resize(window.innerWidth, window.innerHeight);
 
-let b = new Bump(PIXI);
 let TextureCache = PIXI.utils.TextureCache
-let texture = TextureCache["./img/hat.png"];
 let renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
-let state = menu;
-
-let controlText;
-let menuContainer = new PIXI.Container();
-let gameContainer = new PIXI.Container();
-let bulletList = [];
 
 window.onload = function() {
-    document.body.appendChild(app.view);
-    setGamepadConnectionEvents();
-    app.renderer.backgroundColor = 0xd3cb81;
-    setup();
+	document.body.appendChild(app.view);
+	app.renderer.backgroundColor = 0xd3cb81;
+	setup();
 }
 
 
 function setup() {
-    PIXI.loader
-        .add('hat', "./img/hat.png")
-        .add('crosshair', './img/crosshair.png')
-        .add('star', './img/star.png')
-        .add('bullet', './img/bullet.png')
-        .load(onLoadAssets);
+	PIXI.loader
+		.add('hat', "./img/hat.png")
+		.add('white crosshair', './img/white crosshair.png')
+		.add('black crosshair', './img/black crosshair.png')
+		.add('star', './img/star.png')
+		.add('x', './img/x.png')
+		.add('rt', './img/right trigger.png')
+		.add('lstick', './img/left stick.png')
+		.add('rstick', './img/right stick.png')
+		.add('bullet', './img/bullet.png')
+		.add('sand', './img/sand.png');
+
+	//Change if more or less than two characters are playable
+	loadCowboy('white');
+	loadCowboy('black');
+
+	PIXI.loader.load(startManager);
 }
 
-function onLoadAssets() {
-    InitMenu();
-    app.ticker.add(delta => gameLoop(delta))
-}
+//**TODO** Loading screen scene. Progress bar can be changed through different steps of setup?
+//Load all of the cowboys textures for a certain color of cowboy
+//Textures are stored with a name like white_N_walk2
+//Walk cycle would go walk9 --> walk0 and back again.
+function loadCowboy(color) {
+	let directions = ['N', 'S', 'E', 'W', 'NE', 'NW', 'SE', 'SW'];
 
-function gameLoop(delta) {
+	//Loop through all directions to get all images
+	for (let i = 0; i < directions.length; i++) {
+		//./img/cowboys/color/direction_action
+		//Load idle texture for this direction. Frame number is always 0
+		PIXI.loader.add(color + '_' + directions[i] + '_' + 'idle0',
+						'./img/cowboys/' + color + '/' + directions[i] + '_' + 'idle.png');
 
-    state(delta);
-}
+		//Load all of the walking textures for this direction
+		for (let j = 0; j < 10; j++) {
+			PIXI.loader.add(color + '_' + directions[i] + '_walk' + j,
+							'./img/cowboys/' + color + '/' + directions[i] + '_walk' + j + '.png');
+		}
 
-function play(delta) {
-    pollGamepads();
-
-    for (let i = 0; i < players.length; i++) {
-        players[i].update(delta);
-    }
-    for (let i = 0; i < bulletList.length; i++) {
-        bulletList[i].move(delta);
-
-    }
-
-}
-
-function menu(delta) {
-    controlText.text = "Controllers Connected: " + numPads;
-}
-
-function AddBullet(bullet) {
-    bulletList.push(bullet);
-    gameContainer.addChild(bullet.sprite);
-}
-
-
-function SwitchState() {
-    if (state == menu) {
-        if (numPads == 0) {
-            let warnText = new PIXI.Text('Please connect at least 1 controller to play.');
-            warnText.position.set(controlText.x, controlText.y + 50);
-            menuContainer.addChild(warnText);
-            return;
-        }
-
-        InitGame(numPads > 2 ? 2 : numPads);
-        app.stage.removeChild(menuContainer);
-        app.stage.addChild(gameContainer);
-        state = play;
-
-    } else if (state == play) {
-        app.stage.removeChild(gameContainer);
-        app.stage.addChild(menuContainer);
-        state = menu;
-    }
-}
-
-function InitMenu() {
-    //Title Message
-    let style = new PIXI.TextStyle({
-        fontFamily: "Tahoma",
-        fontSize: 100,
-        fill: "black",
-        stroke: 'white',
-        strokeThickness: 4,
-        dropShadow: true,
-        dropShadowColor: "#000000",
-        dropShadowBlur: 4,
-        dropShadowAngle: Math.PI / 6,
-        dropShadowDistance: 6,
-    });
-
-    let titleText = new PIXI.Text("2 Wild 2 West", style);
-    titleText.anchor.set(0.5);
-    titleText.position.set(renderer.width / 2, 400);
-    menuContainer.addChild(titleText);
-
-    //Controller Status
-    controlText = new PIXI.Text("Controllers Connected: " + numPads);
-    controlText.position.set(100, renderer.height - (renderer.height / 6));
-    app.stage.addChild(controlText);
-    menuContainer.addChild(controlText);
-
-    //Star Sprite
-    let button = new PIXI.Sprite(PIXI.loader.resources["star"].texture);
-    button.scale.x = 0.2;
-    button.scale.y = 0.2;
-    button.x = -button.width / 2;
-    button.y = -110;
-
-    //Button Text
-    style.fontSize = 50;
-    let buttonMessage = new PIXI.Text("Start", style);
-    buttonMessage.anchor.set(0.5);
-
-    //Add to container and positioning
-    let buttonContainer = new PIXI.Container();
-    buttonContainer.addChild(button);
-    buttonContainer.addChild(buttonMessage);
-    buttonContainer.x = renderer.width / 2;
-    buttonContainer.y = 600;
-
-    //Make it interactable
-    buttonContainer.interactive = true;
-    buttonContainer.buttonMode = true;
-    buttonContainer.on('pointerdown', SwitchState);
-
-    menuContainer.addChild(buttonContainer);
-
-    app.stage.addChild(menuContainer);
-}
-
-function InitGame(numPlayers) {
-    if (numPlayers == 0) {
-        return;
-    }
-
-    //Init all players
-    for (let i = 0; i < numPlayers; i++) {
-        players[i] = new Cowboy(i + 1);
-        gameContainer.addChild(players[i].sprite);
-    }
+		//Load the shooting textures for this direction
+		for (let j = 0; j < 3; j++) {
+			PIXI.loader.add(color + '_' + directions[i] + '_' + 'shoot' + j,
+							'./img/cowboys/' + color + '/' + directions[i] + '_' + 'shoot' + j + '.png');
+		}
+	}
 }
